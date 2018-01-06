66ce905c34dac7f3c98351ce8e16a8a3f863bacd
Tien Manh Do <domanhtien2011@gmail.com>
2018-01-06 16:27:41 +0700

    Adding should-matchers gem

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -2,8 +2,22 @@ class Achievement < ApplicationRecord
   belongs_to :user
   enum privacy: %i[public_access private_access friends_access]
   validates :title, presence: true
+  validates_presence_of :user, on: :create, message: "can't be blank"
+  # validates_uniqueness_of :title,
+  # on: :create,
+  # message: 'must be unique'
+
+  validate :unique_title_for_one_user
 
   def description_html
     Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(tescription)
+  end
+
+  private
+
+  def unique_title_for_one_user
+    existing_achievement = Achievement.find_by(title: title)
+    return if !existing_achievement || existing_achievement.user != user
+    errors.add(:title, 'You cannot have two achievements with the same title')
   end
 end
--- spec/models/achievement_spec.rb
+++ spec/models/achievement_spec.rb
@@ -1,5 +1,54 @@
 require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'requires title' do
+      achievement = Achievement.new(title: '')
+      # achievement.valid?
+      # expect(achievement.errors[:title]).to include("can't be blank")
+      # expect(achievement.errors[:title]).not_to be_empty
+      expect(achievement.valid?).to be_falsy
+    end
+
+    it 'requires title to be unique for one user' do
+      user = FactoryBot.create(:user)
+      first_achievement = FactoryBot.create(:public_achievement,
+                                            title: 'First achievement',
+                                            user: user)
+      second_achievement = Achievement.new(title: 'First achievement',
+                                           user: user)
+      expect(second_achievement.valid?).to be_falsy
+    end
+
+    it 'allows different users to have identical titles' do
+      user1             = FactoryBot.create(:user)
+      user2             = FactoryBot.create(:user)
+      first_achievement  = FactoryBot.create(:public_achievement,
+                                             title: 'First achievement',
+                                             user: user1)
+      second_achievement = Achievement.new(title: 'First achievement',
+                                           user: user2)
+      expect(second_achievement.valid?).to be_truthy
+    end
+
+    it 'belongs to a user' do
+      achievement = Achievement.new(title:
+      'I am the toughest and smartest Navy SEALS',
+                                    user: nil)
+      expect(achievement.valid?).to be_falsy
+    end
+
+    # it 'has belong_to user association' do
+    #   # 1st approach
+    #   user = FactoryBot.create(:user)
+    #   achievement = FactoryBot.create(:public_achievement, user: user)
+    #   expect(achievement.user).to eq(user)
+
+    #   # 2nd approach
+    #   u = Achievement.reflect_on_association(:user)
+    #   expect(u.macro).to eq(:belongs_to)
+    # end
+
+    it { should belong_to(:user) }
+  end
 end
